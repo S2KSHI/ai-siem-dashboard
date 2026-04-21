bda96fbcfaf2ab71efe6675bbfa2cfa3c5541a8c
S2KSHI <sg0438462@gmail.com>
2026-04-22 01:55:28 +0530

    Fix port binding for Railway deployment

--- app_groq.py
+++ app_groq.py
@@ -707,7 +707,5 @@ def start_background_services():
     sniffing_thread = threading.Thread(target=start_sniffing, daemon=True)
     sniffing_thread.start()
 
-
-if __name__ == '__main__':
-    start_background_services()
-    socketio.run(app, debug=True, port=5000, use_reloader=False, allow_unsafe_werkzeug=True)
+port = int(os.environ.get("PORT", 5000))
+socketio.run(app, host="0.0.0.0", port=port, debug=False, use_reloader=False, allow_unsafe_werkzeug=True)
